3be5bc03d83dc1445f85216d0c579229709a1b69
William Anderson <willjatlas@googlemail.com>
2020-12-30 16:54:33 +0000

    init

---
+++ src/components/NavDisplay.js
@@ -0,0 +1,15 @@
+
+const NavDisplay = ({setPlatform}) => {
+
+    return (
+        <>
+            <h1>Warframe News</h1>
+            <button> PC </button>
+            <button> PS4 </button>
+            <button> XBox1 </button>
+            <button> Switch </button>
+        </>
+    );
+
+}
+export default NavDisplay;
