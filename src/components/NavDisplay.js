360de6dcae8bbd572d983e032759861d84118e44
William Anderson <willjatlas@googlemail.com>
2020-12-30 17:17:42 +0000

    nav bar outline done

--- src/components/NavDisplay.js
+++ src/components/NavDisplay.js
@@ -2,13 +2,13 @@
 const NavDisplay = ({setPlatform}) => {
 
     return (
-        <>
+        <div id="nav-display">
             <h1>Warframe News</h1>
-            <button> PC </button>
-            <button> PS4 </button>
-            <button> XBox1 </button>
-            <button> Switch </button>
-        </>
+            <button class="nav-button"> PC </button>
+            <button class="nav-button"> PS4 </button>
+            <button class="nav-button"> XBox1 </button>
+            <button class="nav-button"> Switch </button>
+        </div>
     );
 
 }
