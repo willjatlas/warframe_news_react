75ee976fffb8275bb1d1dc4a5427fd2a0c9f0dfc
William Anderson <willjatlas@googlemail.com>
2021-01-03 12:57:20 +0000

    logo added

--- src/components/NavDisplay.js
+++ src/components/NavDisplay.js
@@ -1,13 +1,15 @@
-
+import wfLogo from "../wfLogo.png";
 const NavDisplay = ({handleButton}) => {
 
     return (
         <div id="nav-display">
+            <img id="logo-img" src={wfLogo} alt="Warframe Logo" />
             <h1>Warframe News</h1>
             <button id="nav-button" onClick={handleButton} value="pc" > PC </button>
             <button id="nav-button" onClick={handleButton} value="ps4"> PS4 </button>
             <button id="nav-button" onClick={handleButton} value="xb1"> XBox1 </button>
             <button id="nav-button" onClick={handleButton} value="swi"> Switch </button>
+            <img id="logo-img" src={wfLogo} alt="Warframe Logo" />
         </div>
     );
 
